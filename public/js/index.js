a042e69868586d40ba73716f78608c3254f248df
devbaweja <18ucs081@lnmiit.ac.in>
2020-05-28 16:57:28 +0530

    Alert

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
